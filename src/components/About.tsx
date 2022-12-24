cb957fb9f5f65a0ceba1cd49f28f7d1eada6030f
Vaenthan Jeevarajah <v2jeevar@uwaterloo.ca>
2022-12-24 14:43:26 -0500

    Updating content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,24 +6,24 @@ import "./About.scss"
 
 function About() {
     return (
-    <section id="about" className="h-screen flex content-center">
-      <div className="container mx-auto flex md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="font-title text-5xl mb-10 font-medium text-gray-900">
+    <section id="about" className="flex content-center md:h-screen">
+      <div className="container mx-auto flex md:flex-row items-center">
+        <div className="sm:text-left break-normal mx-4 lg:flex-grow flex flex-col md:items-start">
+          <h1 className="font-title text-5xl mb-10 font-medium text-gray-900 mt-8 md:pt-0">
             Vaenthan Jeevarajah
           </h1>
           <p className="mb-8 leading-relaxed text-lg text-gray-700">
-            Hey! &#128075;&#127998; I'm Vaenthan, a 3rd year Software Engineering student at the University of Waterloo with an interest
+            Hey! &#128075;&#127998; I'm Vaenthan, a 4th year Software Engineering student at the University of Waterloo with an interest
             in full stack development, design, and machine learning and a passion for solving problems.
           </p>
           <p className="mb-8 leading-relaxed text-lg text-gray-700">
-            Previously, I completed co-ops for <a className="KPMG" href="https://home.kpmg/ca/en/home/services/tax/one-port.html" target="_blank" rel="noopener noreferrer">KPMG</a> and <a className="AMD" href="https://www.amd.com/en" target="_blank" rel="noopener noreferrer">AMD</a>.
+            Previously, I completed co-ops for <a className="GEM" href="https://www.gem.com/" target="_blank" rel="noopener noreferrer">Gem</a>, <a className="KPMG" href="https://home.kpmg/ca/en/home/services/tax/one-port.html" target="_blank" rel="noopener noreferrer">KPMG</a>, and <a className="AMD" href="https://www.amd.com/en" target="_blank" rel="noopener noreferrer">AMD</a>.
             Feel free to reach out! Always looking to work on something interesting. 
           </p>
           <p className="mb-8 leading-relaxed text-lg text-gray-700 font-bold">
-            Currently seeking Spring 2022 Internships. 
+            Currently seeking Winter 2023 Internships. 
           </p>
-          <div className="inline text-4xl space-x-6 text-gray-400">
+          <div className="inline text-4xl space-x-6 text-gray-400 mb-8 mb:pt-0">
             <a href="mailto:v2jeevar@uwaterloo.ca" className="hover:text-gray-700">
                 <FontAwesomeIcon icon={faEnvelope} href="mailto:v2jeevar@uwaterloo.ca"/>
             </a>
